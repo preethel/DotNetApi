99ccf0f0bd8fd4f166679f7db0e92fd9f402da15
azam1 <azam1@ZIDDISPC>
2022-06-23 16:00:01 +0600

    Add project files.

---
+++ DotNetApi/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public string MyName()
+        {
+            return "Redwan";
+        }
+        [HttpGet]
+        public string MyFullName(){
+            return "Md. Nur A Redwan Ul Azam";
+        }
+    }
+}
---
+++ DotNetApi/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DotNetApi.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Please Enter title.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Please Enter title.")]
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+    }
+}
